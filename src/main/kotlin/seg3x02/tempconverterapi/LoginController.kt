73c154c49f0edb032cc80d9911bb52b3622f2b40
lisakorolyov <ekoro061@uottawa.ca>
2024-11-17 01:02:35 -0800

    Added login and logout functionality

---
+++ src/main/kotlin/seg3x02/tempconverterapi/LoginController.kt
@@ -0,0 +1,13 @@
+package seg3x02.tempconverterapi
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class LoginController: WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/login").setViewName("login")
+        registry.addViewController("/home").setViewName("home")
+    }
+}
